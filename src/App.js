7163f49978a7f9169118bf3bccc84d10d36ce007
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-18 14:12:57 -0300

    chore: add react-navigation/native

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar backgroundColor="#030404" barStyle="light-content" />
+      <View style={{ flex: 1, backgroundColor: "#030404" }}>
+        <Text> Recriando a interface do picpay</Text>
+      </View>
+    </>
+  );
+}

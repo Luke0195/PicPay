e29a00c4eb0b4822a4266d530e8099d3401779f1
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-18 15:09:00 -0300

    chore: add styled-components

---
+++ src/components/PayButton/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function PayButton() {
+  return (
+    <>
+      <Text style={{ color: "#fff" }}> Payment </Text>
+    </>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,113 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AntDesignIcons from "react-native-vector-icons/AntDesign";
+
+import IconiconsIcons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "../pages/Home";
 import WalletScreen from "../pages/Wallet";
 import PaymentScreen from "../pages/Payment";
 import NotificationsScreen from "../pages/Notifications";
 import SettingsScreen from "../pages/Ajusts";
+import PayButton from "../components/PayButton";
 
 const Tab = createBottomTabNavigator();
 
+const icones = {
+  Home: {
+    lib: AntDesignIcons,
+    name: "home",
+  },
+
+  Wallet: {
+    lib: AntDesignIcons,
+    name: "creditcard",
+  },
+
+  Pay: {
+    lib: AntDesignIcons,
+    name: "pay",
+  },
+
+  Notifications: {
+    lib: IconiconsIcons,
+    name: "notifications-outline",
+  },
+
+  Setting: {
+    lib: AntDesignIcons,
+    name: "settings",
+  },
+};
+
 export default function Navigation() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Wallet" component={WalletScreen} />
-      <Tab.Screen name="Payment" component={PaymentScreen} />
-      <Tab.Screen name="Notifications" component={NotificationsScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Navigator
+      tabBarOptions={{
+        style: {
+          backgroundColor: "#131418",
+          borderTopColor: "rgba(255,255,255, 0.2)",
+        },
+        activeTintColor: "#fff",
+        inactiveTintColor: "#92929c",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          // eslint-disable-next-line react/prop-types
+          tabBarIcon: ({ color, size }) => (
+            <AntDesignIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Wallet"
+        component={WalletScreen}
+        options={{
+          // eslint-disable-next-line react/prop-types
+          tabBarIcon: ({ color, size }) => (
+            <AntDesignIcons name="creditcard" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Payment"
+        component={PaymentScreen}
+        options={{
+          // eslint-disable-next-line react/prop-types
+          title: "",
+
+          tabBarIcon: ({ color, size }) => (
+            <PayButton name="pay" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationsScreen}
+        options={{
+          // eslint-disable-next-line react/prop-types
+          tabBarIcon: ({ color, size }) => (
+            <IconiconsIcons
+              name="notifications-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          // eslint-disable-next-line react/prop-types
+          tabBarIcon: ({ color, size }) => (
+            <AntDesignIcons name="setting" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }

9e3002a85b2951748a5f7ed9e102533e9aefda2b
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-20 15:45:27 -0300

    feat: add Wallet Screen

--- src/pages/Wallet/index.js
+++ src/pages/Wallet/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch } from "react-native";
+
 import FeatherIcons from "react-native-vector-icons/Feather";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import AntDesign from "react-native-vector-icons/AntDesign";
 import MaterialIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import cardImage from "../../images/credit-card.png";
+
 import {
   Wrapper,
   Header,
@@ -15,18 +21,38 @@ import {
   Actions,
   Action,
   ActionLabel,
+  UserBalance,
+  UserBalanceTitle,
+  PaymentMethods,
+  PaymentMethodsTitle,
+  Card,
+  CardImage,
+  CardText,
+  CardContent,
+  CardTitle,
+  CardContainer,
+  AddContainer,
+  AddLabel,
+  AddButton,
+  TicketContainer,
+  TicketText,
 } from "./styles";
 
 export default function Wallet() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleToggleVisible() {
+    setIsVisible((prevState) => !prevState);
+  }
   return (
     <Wrapper>
       <Header colors={["#52e78c", "#1aB563"]}>
         <HeaderContainer>
-          <Title> Seu Saldo PicPay</Title>
+          <Title> Seu Saldo PayNow</Title>
           <BalanceContainer>
             <Balance>
               R$
-              <BalanceBold> 0, 00</BalanceBold>
+              <BalanceBold> {isVisible ? "78.871,63" : "-------"}</BalanceBold>
             </Balance>
             <EyeButton>
               <FeatherIcons name="eye" size={23} color="#fff" />
@@ -46,6 +72,35 @@ export default function Wallet() {
           </Actions>
         </HeaderContainer>
       </Header>
+      <UserBalance>
+        <UserBalanceTitle> Usar Saldo ao pagar </UserBalanceTitle>
+        <Switch />
+      </UserBalance>
+      <PaymentMethods>
+        <PaymentMethodsTitle> formas de pagemento </PaymentMethodsTitle>
+        <Card>
+          <CardContainer>
+            <CardContent>
+              <CardTitle> Cadastre seu cartão de crédito</CardTitle>
+              <CardText>
+                Cadastre seu cartão de crédito para poder fazer pagamentos mesmo
+                quando não tiver saldo no seu PayNow.
+              </CardText>
+            </CardContent>
+            <CardImage source={cardImage} resizeMode="contain" />
+          </CardContainer>
+          <AddContainer>
+            <AddButton>
+              <AntDesign name="pluscircleo" size={30} color="#0d8060" />
+              <AddLabel> Adicionar uma conta</AddLabel>
+            </AddButton>
+          </AddContainer>
+        </Card>
+      </PaymentMethods>
+      <TicketContainer>
+        <MaterialIcons name="ticket-outline" color="#0d8060" size={25} />
+        <TicketText> Usar código promocial</TicketText>
+      </TicketContainer>
     </Wrapper>
   );
 }
--- src/pages/Wallet/styles.js
+++ src/pages/Wallet/styles.js
@@ -54,7 +54,7 @@ export const Actions = styled.View`
   margin-top: 25px;
   align-items: center;
   justify-content: space-between;
-  width: 300px;
+  width: 280px;
 `;
 export const Action = styled.TouchableOpacity`
   flex-direction: row;
@@ -62,12 +62,102 @@ export const Action = styled.TouchableOpacity`
   border: 1px solid rgba(255, 255, 255, 0.5);
   width: 120px;
   height: 45px;
-  padding: 0 5px;
+  padding: 10px;
   justify-content: space-around;
-  border-radius: 8px;
+  border-radius: 20px;
 `;
 export const ActionLabel = styled.Text`
   color: #fff;
   font-size: 16px;
   font-family: "Roboto-Regular";
+`;
+
+export const UserBalance = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background: #1c1c1e;
+  height: 60px;
+  padding: 0 10px;
+`;
+
+export const UserBalanceTitle = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-family: "Roboto-Medium";
+`;
+
+export const PaymentMethods = styled.View`
+  margin-top: 25px;
+  padding: 0 16px;
+`;
+export const PaymentMethodsTitle = styled.Text`
+  color: #8e8e93;
+  font-size: 14px;
+  font-family: "Roboto-Regular";
+  text-transform: uppercase;
+  margin-bottom: 10px;
+`;
+
+export const Card = styled.View`
+  background: #1e2324;
+  padding: 15px;
+  height: 150px;
+  border-radius: 8px;
+`;
+export const CardContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const CardContent = styled.View`
+  flex: 1;
+  margin-right: 14px;
+`;
+export const CardTitle = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  text-align: left;
+`;
+export const CardText = styled.Text`
+  color: rgba(255, 255, 255, 0.6);
+  margin-top: 8px;
+  font-size: 14px;
+`;
+
+export const CardImage = styled.Image`
+  width: 60px;
+  height: 60px;
+`;
+
+export const AddContainer = styled.View`
+  margin: 14px 0 8px 5px;
+`;
+
+export const AddButton = styled.TouchableOpacity`
+  flex-direction: row;
+
+  align-items: center;
+`;
+
+export const AddLabel = styled.Text`
+  margin-left: 10px;
+  font-family: "Roboto-Medium";
+  font-size: 18px;
+  color: #0d8060;
+`;
+
+export const TicketContainer = styled.TouchableOpacity`
+  margin-top: 20px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TicketText = styled.Text`
+  font-size: 18px;
+  color: #0d8060;
+  padding-bottom: 2px;
+  border-bottom-width: 1px;
+  border-color: #0ee3a8;
 `;

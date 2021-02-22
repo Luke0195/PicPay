8cab3c91697250e4413028c6ae6e91066d8f9563
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-22 19:00:27 -0300

    feat: initial start of notification screen

--- src/pages/Notifications/index.js
+++ src/pages/Notifications/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Wrapper, NotificationsTitle, Container } from "./styled";
 
 export default function Notifications() {
   return (
-    <View>
-      <Text> Notifications Screen</Text>
-    </View>
+    <Container>
+      <Wrapper>
+        <NotificationsTitle> Notificações </NotificationsTitle>
+      </Wrapper>
+    </Container>
   );
 }
---
+++ src/pages/Notifications/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background: #030404;
+`;
+
+export const Wrapper = styled.View`
+  margin-top: 25px;
+`;
+
+export const NotificationsTitle = styled.Text`
+  font-size: 17px;
+  color: #fff;
+  text-align: center;
+  font-family: "Roboto-Medium";
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`;

596f359fc2e359b682166c18821ffbb1edbb93a1
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-23 14:10:07 -0300

    feat: add notification screen

--- src/pages/Notifications/index.js
+++ src/pages/Notifications/index.js
@@ -1,12 +1,76 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Wrapper, NotificationsTitle, Container } from "./styled";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import {
+  Wrapper,
+  NotificationsTitle,
+  Container,
+  IconsContainer,
+  IconContent,
+  IconImage,
+  IconLabel,
+  IconText,
+  IconTitle,
+} from "./styled";
 
+import peopleImg from "../../images/people.png";
+import moneyImg from "../../images/money.png";
+import avatar from "../../images/avatar.png";
+import creditImg from "../../images/credit-card.png";
+import recargaImg from "../../images/11.png";
+
+const icons = [
+  {
+    key: String(Math.random()),
+    name: "Pagar Pessoas",
+    description: "Pague todos os seus amigos que utilizam o paynow",
+    img: peopleImg,
+  },
+
+  {
+    key: String(Math.random()),
+    name: "Pagar Com dinheiro",
+    description: "Pague todos os seus amigos que com dinheiro  o paynow",
+    img: moneyImg,
+  },
+
+  {
+    key: String(Math.random()),
+    name: "Cobrar",
+    description: "Cobrar um amigo",
+    img: creditImg,
+  },
+
+  {
+    key: String(Math.random()),
+    name: "Alterar suas Informações",
+    description: "Atualize seu perfil no paynow ",
+    img: avatar,
+  },
+
+  {
+    key: String(Math.random()),
+    name: "Recarga de Celular",
+    description: "Vivo, Tim, Claro, Oi,Correiros",
+    img: recargaImg,
+  },
+];
 export default function Notifications() {
   return (
     <Container>
       <Wrapper>
         <NotificationsTitle> Notificações </NotificationsTitle>
+        <IconsContainer>
+          {icons.map((icon) => (
+            <IconContent key={icon.key}>
+              <IconImage source={icon.img} resizeMode="contain" />
+              <IconText>
+                <IconTitle> {icon.name}</IconTitle>
+                <IconLabel>{icon.description}</IconLabel>
+              </IconText>
+              <AntDesign name="arrowright" size={23} color="#fff" />
+            </IconContent>
+          ))}
+        </IconsContainer>
       </Wrapper>
     </Container>
   );
--- src/pages/Notifications/styled.js
+++ src/pages/Notifications/styled.js
@@ -1,11 +1,11 @@
 import styled from "styled-components/native";
 
-export const Container = styled.ScrollView`
+export const Container = styled.SafeAreaView`
   flex: 1;
   background: #030404;
 `;
 
-export const Wrapper = styled.View`
+export const Wrapper = styled.ScrollView`
   margin-top: 25px;
 `;
 
@@ -16,4 +16,43 @@ export const NotificationsTitle = styled.Text`
   font-family: "Roboto-Medium";
   text-transform: uppercase;
   letter-spacing: 1px;
+`;
+
+export const IconsContainer = styled.View`
+  margin-top: 25px;
+  padding: 0 16px;
+`;
+
+export const IconContent = styled.View`
+  margin-top: 25px;
+  background: #1e222b;
+  padding: 20px 8px;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 8px;
+`;
+
+export const IconImage = styled.Image`
+  width: 50px;
+  height: 50px;
+`;
+
+export const IconText = styled.View`
+  flex: 1;
+  margin-left: 15px;
+`;
+
+export const IconTitle = styled.Text`
+  color: #fff;
+  font-family: "Roboto-Regular";
+  font-size: 17px;
+  text-align: left;
+`;
+
+export const IconLabel = styled.Text`
+  color: rgba(255, 255, 255, 0.7);
+  font-size: 14px;
+  text-align: left;
+  font-family: "Roboto-Light";
+  margin-top: 4px;
 `;
